5b927cb0696d4dddbabed374db8e80c2a3fc736c
Jose Corredor <jose96corrza@gmail.com>
2023-08-30 00:29:19 -0500

    Initial commit for pos electron app

---
+++ src/main.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, '../src/preload/preload.js') // Will run this script before
+            //To "attach"both renderer globals, window and document
+        }
+    })
+
+    // win.loadFile('./public/index.html')
+    win.loadFile(`${path.join(__dirname, '../public/index.html')}`)
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})
